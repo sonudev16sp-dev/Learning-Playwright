907d1f87c9436eef04271dbe2c8061b28d4c8359
sonudev16sp-dev <sonudev16.sp@gmail.com>
2026-02-28 15:35:35 +0530

    Add files via upload

---
+++ Playwright_practice/Increment&Decrement_Operator.js
@@ -0,0 +1,8 @@
+let a = 5; 
+console.log("Value of a befor increment :"+a); 
+let b = a++ + ++a;
+console.log("Value of b befor increment :"+b); // Output: 5
+let c = a++ + ++a + ++a + ++a + a++;
+console.log("Value of c befor increment :"+c); // Output: 5
+
+
